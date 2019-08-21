0bcd1c01ca65b0d4632cdefa81cb889b81d9398e
verdantpath <m_dittmann@yahoo.com>
2019-08-20 20:09:54 -0700

    add bind, update, unbind

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
-Vue.directive('square', function (el, binding) {
-  el.innerHTML = Math.pow(binding.value, 2);
-});
-
-new Vue({
-  el: '#app',
-  data: { item: 42 }
+Vue.directive('my-directive', {
+  bind: function() {
+    // do the preparation work on element binding
+  }, 
+  update: function() {
+    // do something based on the updated value
+  },
+  unbind: function() {
+    // do the clean up work
+  }
 });
